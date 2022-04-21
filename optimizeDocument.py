8c62a4f089aae6b9c3fcc0bfd97b89f98c1ef59b
mkricke <maximilian.kricke@algonaut-berlin.de>
2022-04-21 18:45:33 +0200

    init file added

--- optimizeDocument.py
+++ optimizeDocument.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 import math
 import pandas as pd
@@ -5,8 +6,11 @@ import gensim
 from gensim import corpora
 import scipy
 from scipy import optimize
-
-# %%
-
+from init import *
 #%% 
-data = pd.
+data = pd.read_csv('poliblogs2008.csv')
+
+#%%
+documents = corpora.MmCorpus('corpus.mm')
+dictionary = corpora.Dictionary.load('dictionary')
+vocab = dictionary.token2id
